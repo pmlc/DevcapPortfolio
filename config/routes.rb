37f7e562ccd501272a0c2ecb8286ee5a6bff8df1
Pierre Le Count <pmlc@iinet.net.au>
2018-07-24 01:26:25 +0000

    customised portofkio show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
-Rails.application.routes.draw do
-  
-  resources :portfolios
- 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+Rails.application.routes.draw do 
+ resources :portfolios, except: [:show] 
+ get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
 
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get 'about-me', to: 'pages#about' 
+ get 'contact', to: 'pages#contact' 
 
-end
+ resources :blogs do 
+   member do 
+       get :toggle_status 
+     end 
+   end 
+    
+   root to: 'pages#home' 
+end 
