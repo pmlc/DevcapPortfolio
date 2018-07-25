5e00d9d10fc7dd79e06c356e20bc3b99fb1a6db7
Pierre Le Count <pmlc@iinet.net.au>
2018-07-25 05:30:11 +0000

    Implemented Concern for managing data - Default Images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725022659) do
+ActiveRecord::Schema.define(version: 20180725050446) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180725022659) do
     t.integer "percent_utilised"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
