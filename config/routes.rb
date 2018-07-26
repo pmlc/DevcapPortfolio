2c915839ce585ff7f4983bae10d632e58dc197d6
Pierre Le Count <pmlc@iinet.net.au>
2018-07-26 22:36:33 +0000

    customise login, logout and registeration

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do 
-  devise_for :users
- resources :portfolios, except: [:show] 
- get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
+  
+  # mske routes for lgin/out less combersome
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  #devise_for :users
+  
+  resources :portfolios, except: [:show] 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
 
- get 'about-me', to: 'pages#about' 
- get 'contact', to: 'pages#contact' 
+  get 'about-me', to: 'pages#about' 
+  get 'contact', to: 'pages#contact' 
  
- resources :blogs do
-   member do
-     get :toggle_status
-   end
- end
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
- root to: 'pages#home' 
+  root to: 'pages#home' 
 end 
