2b8e7027faac655331151274d5f0decb171e638b
Pierre Le Count <pmlc@iinet.net.au>
2018-07-30 02:23:20 +0000

    Somehow mixup with unsaved code this is for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  # currently need to comment this out
-  # include DeviseWhitelist
-  
-  # remove below when above works
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  def configure_permitted_parameters 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) 
-  end 
-  
+  include DeviseWhitelist
+  include SetSource 
+  include CurrentUserConcern 
+
 end
